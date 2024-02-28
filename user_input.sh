cee9bd3cb4c20906eb94b95fb0d1af10e2c5e966
ebendel <108996443+ebendel@users.noreply.github.com>
2024-02-27 22:55:23 -0500

    Create user_input.sh
    Bash script for user input

---
+++ user_input.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo " ###################################################"
+echo "Enter your skills"
+read SKILLS 
+
+echo "###################################################"
+echo "Your $SKILLS skill is in high demand in IT Industry"
+
+
+echo "#### Prompt a message and store response from the user in variables USR and pass ######"
+read -p 'Username: ' USR
+read -sp 'Password: ' pass
+
+echo 
+
+echo "Login Successful: Welcome USER $USR,"
